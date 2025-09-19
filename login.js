3ec7acd438241fcd3ea3eefcb13709fa2b1bf198
Sula-2003 <165894514+Sula-2003@users.noreply.github.com>
2025-09-19 16:14:23 +0530

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-let backward=document.querySelector("#backward");
-backward.addEventListener("click",()=>{
-    history.back();
-})
-let forward=document.querySelector("#forward");
-forward.addEventListener("click",()=>{
-    history.forward();
-})
